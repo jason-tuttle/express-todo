1e5e5295f5974f587cdefffa11c5ae94af290046
Jason Tuttle <jason@hearspotbark.com>
2017-06-21 16:01:40 -0400

    functional

---
+++ index.js
@@ -0,0 +1,48 @@
+/* module imports *************************************************************/
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+const mustacheExpress = require('mustache-express');
+
+const app = express();
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+
+// parse all requests using the generic body parser (req.body is now available)
+app.use(bodyParser.urlencoded({extended: true}));
+// gives us a way to validate input (e.g., ensure emails are valid)
+app.use(expressValidator());
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+const todos = [
+  { id: 0, item: "Wake up in the morning", completed: false },
+  { id: 1, item: "Take the 8:15 to the city", completed: false },
+  { id: 2, item: "Start your slaving job to get your pay", completed: false }
+];
+
+app.get('/', function(req, res) {
+  res.render('index', { todos: todos });
+})
+
+app.post('/', function(req, res) {
+  req.checkBody('item', 'Add some text first').notEmpty();
+  const errors = req.validationErrors();
+  if (errors) {
+    // just return the whole array as a string
+    res.send(errors);
+  } else {
+    todos.push({ id: todos.length, item: req.body.item, completed: false });
+    res.render('index', { todos: todos });
+  }
+});
+
+app.post('/list', function(req, res) {
+  todos[req.body.check].completed = true;
+  res.render('index', { todos: todos });
+});
+
+app.listen(3030, () => console.log('Servin\'...'));
